62f354e7341496442e5708d734e3fe3893cd70b9
tjcjones <tjcjones@8a9c3130-423d-464b-8744-2863c99db41f>
2009-06-19 16:20:08 +0000

    Better fix for buffer overflow
    git-svn-id: https://sshguard.svn.sourceforge.net/svnroot/sshguard/trunk@103 8a9c3130-423d-464b-8744-2863c99db41f

--- src/sshguard_procauth.c
+++ src/sshguard_procauth.c
@@ -75,16 +75,11 @@ int procauth_fin() {
 int procauth_addprocess(char *conf) {
     procpid pp;
     int srvcode;
-    char pidfilename[FILENAME_MAX], *p;
+    char pidfilename[FILENAME_MAX];
 
     /* ensure the filename length is within system limit */ 
-    if ((p = memchr(conf, ':', FILENAME_MAX)) == NULL)
+    if (memchr(conf, '\0', FILENAME_MAX) == NULL)
 	return -1;
-
-    if (strnlen(p, FILENAME_MAX) == FILENAME_MAX) {
-	sshguard_log(LOG_ERR, "supplied path and/or filename exceeds system length limit");
-	return -1;
-    }
 
     /* conf format:     service_code:pid_filename   */
     if (sscanf(conf, "%d:%s", &srvcode, pidfilename) != 2)
