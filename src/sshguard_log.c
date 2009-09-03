cb9178b35b55005f5852171557bf4b5e5d83940f
mijio <mijio@8a9c3130-423d-464b-8744-2863c99db41f>
2009-09-03 17:52:21 +0000

    implement auto-resizeable msg buffer; avoid vsyslog() for compatibility
    git-svn-id: https://sshguard.svn.sourceforge.net/svnroot/sshguard/trunk@134 8a9c3130-423d-464b-8744-2863c99db41f

--- src/sshguard_log.c
+++ src/sshguard_log.c
@@ -21,31 +21,70 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 
 #include "sshguard_log.h"
 
-int sshg_log_debugging;
+static int sshg_log_debugging;
+
+static char *msgbuf = NULL;
+static size_t msgbuf_len;
+/* when the buffer is too little, how much bigger do we make it? (factor, 0..+oo) */
+static const float msgbuf_growth_factor = 0.2;
+/* if msgbuf would get bigger than this, just give up increasing (bytes) */
+static const size_t msgbuf_max_length = 1024;
+
 
 int sshguard_log_init(int debugmode) {
     sshg_log_debugging = debugmode;
     if (! sshg_log_debugging) openlog("sshguard", LOG_PID, LOG_AUTH);
+
+    /*
+     * Prepare the message buffer. Start with 100 bytes,
+     * will increase automatically if too small.
+     */
+    msgbuf_len = 100;
+    msgbuf = (char *)malloc(msgbuf_len);
+    assert(msgbuf != NULL);
+    assert(msgbuf_growth_factor > 0);
+
     return 0;
 }
 
 /* finalize the given logging subsystem */
 int sshguard_log_fin() {
     if (! sshg_log_debugging) closelog();
+    free(msgbuf);
     return 0;
 }
 
 int sshguard_log(int prio, char *fmt, ...) {
     va_list ap;
+
+    /* has the logging subsystem been initialized? */
+    assert(msgbuf != NULL);
+
     va_start(ap, fmt);
     if (sshg_log_debugging) {
         vfprintf(stderr, fmt, ap);
         if (fmt[strlen(fmt)-1] != '\n') fprintf(stderr, "\n");
-    } else
-        vsyslog(prio, fmt, ap);
+    } else {
+        /* avoid the more convenient vsyslog() for portability reasons.. */
+        while (vsnprintf(msgbuf, msgbuf_len, fmt, ap) >= msgbuf_len) {
+            /* msgbuf was too small to host message, increase it by 20% and retry */
+            size_t newlen = msgbuf_len + msgbuf_growth_factor * msgbuf_len;
+            if (newlen > msgbuf_max_length) {
+                /* give up, just log a "cut" message.. */
+                break;
+            }
+            free(msgbuf);
+            msgbuf_len = newlen;
+            msgbuf = (char *)malloc(msgbuf_len);
+            assert(msgbuf != NULL);
+        }
+        syslog(prio, "%s", msgbuf);
+    }
     va_end(ap);
 
     return 0;
