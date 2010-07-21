06bd326a48d523a4de98eeee29b4eeda90abd4bf
mijio <mijio@8a9c3130-423d-464b-8744-2863c99db41f>
2010-07-21 06:54:33 +0000

    detect a POSIX grep in system, so IPFilter backend works on Solaris too
    git-svn-id: https://sshguard.svn.sourceforge.net/svnroot/sshguard/trunk@205 8a9c3130-423d-464b-8744-2863c99db41f

--- src/fwalls/command_ipfilter.h
+++ src/fwalls/command_ipfilter.h
@@ -23,9 +23,25 @@
 /* user-define backend ipfilter */
 #include "../config.h"
 
-#define COMMAND_INIT       "grep -qE '^##sshguard-begin##\n##sshguard-end##$' < " IPFILTER_CONFFILE
+/* expanded, it runs something like
+ *      grep -E'^##sshguard-begin##\n##sshguard-end##$' < /etc/ipfilter.conf >/dev/null 2>/dev/null        */
+#define COMMAND_INIT       GREP_PATH "-E '^##sshguard-begin##\n##sshguard-end##$' < " IPFILTER_CONFFILE " >/dev/null 2>/dev/null"
+
 #define COMMAND_FIN        ""
-#define COMMAND_BLOCK      "if test $SSHG_ADDRKIND != 4; then exit 1 ; fi ; TMP=`mktemp /tmp/ipfconf.XXXXX` && awk '1 ; /^##sshguard-begin##$/ { print \"block in quick proto tcp from '\"$SSHG_ADDR\"' to any\" }' <" IPFILTER_CONFFILE " > $TMP && mv $TMP " IPFILTER_CONFFILE " && " IPFPATH "/ipf -Fa && " IPFPATH "/ipf -f " IPFILTER_CONFFILE
-#define COMMAND_RELEASE    "if test $SSHG_ADDRKIND != 4; then exit 1 ; fi ; TMP=`mktemp /tmp/ipfconf.XXXXX` && awk 'BEGIN { copy = 1 } copy ; /^##sshguard-begin##$/    { copy = 0 ; next } !copy { if ($0 !~ /'\"$SSHG_ADDR\"'.*/) print $0 } /^##sshguard-end##$/  { copy = 1 }' <" IPFILTER_CONFFILE " >$TMP && mv $TMP " IPFILTER_CONFFILE " && " IPFPATH "/ipf -Fa && " IPFPATH "/ipf -f " IPFILTER_CONFFILE
-#define COMMAND_FLUSH      "TMP=`mktemp /tmp/ipfconf.XXXXX` && awk 'BEGIN { copy = 1 } /^##sshguard-begin##$/ { print $0 ; copy = 0 } /^##sshguard-end##$/ { copy = 1 } copy' <" IPFILTER_CONFFILE " >$TMP ; mv $TMP " IPFILTER_CONFFILE " ; " IPFPATH "/ipf -Fa && " IPFPATH "/ipf -f " IPFILTER_CONFFILE
+
+/* expanded, it runs something like
+ *      if test $SSHG_ADDRKIND != 4; then exit 1 ; fi ; TMP=`mktemp /tmp/ipfconf.XXXXX` && awk '1 ; /^##sshguard-begin##$/ { print \"block in quick proto tcp from '\"$SSHG_ADDR\"' to any\" }' < /etc/ipfilter.conf > $TMP && mv $TMP /etc/ipfilter.conf && /sbin/ipf -Fa && /sbin/ipf -f /etc/ipfilter.conf
+ */
+#define COMMAND_BLOCK      "if test $SSHG_ADDRKIND != 4; then exit 1 ; fi ; TMP=`mktemp /tmp/ipfconf.XXXXX` && " AWK " '1 ; /^##sshguard-begin##$/ { print \"block in quick proto tcp from '\"$SSHG_ADDR\"' to any\" }' <" IPFILTER_CONFFILE " > $TMP && mv $TMP " IPFILTER_CONFFILE " && " IPFPATH "/ipf -Fa && " IPFPATH "/ipf -f " IPFILTER_CONFFILE
+
+/* expanded, it runs something like
+ *      if test $SSHG_ADDRKIND != 4; then exit 1 ; fi ; TMP=`mktemp /tmp/ipfconf.XXXXX` && awk 'BEGIN { copy = 1 } copy ; /^##sshguard-begin##$/    { copy = 0 ; next } !copy { if ($0 !~ /'\"$SSHG_ADDR\"'.* /) print $0 } /^##sshguard-end##$/  { copy = 1 }' < /etc/ipfilter.conf >$TMP && mv $TMP /etc/ipfilter.conf && /sbin/ipf -Fa && /sbin/ipf -f /etc/ipfilter.conf
+ */
+#define COMMAND_RELEASE    "if test $SSHG_ADDRKIND != 4; then exit 1 ; fi ; TMP=`mktemp /tmp/ipfconf.XXXXX` && " AWK " 'BEGIN { copy = 1 } copy ; /^##sshguard-begin##$/    { copy = 0 ; next } !copy { if ($0 !~ /'\"$SSHG_ADDR\"'.*/) print $0 } /^##sshguard-end##$/  { copy = 1 }' <" IPFILTER_CONFFILE " >$TMP && mv $TMP " IPFILTER_CONFFILE " && " IPFPATH "/ipf -Fa && " IPFPATH "/ipf -f " IPFILTER_CONFFILE
+
+/* expanded, it runs something like
+ *      TMP=`mktemp /tmp/ipfconf.XXXXX` && awk 'BEGIN { copy = 1 } /^##sshguard-begin##$/ { print $0 ; copy = 0 } /^##sshguard-end##$/ { copy = 1 } copy' </etc/ipfilter.conf >$TMP ; mv $TMP /etc/ipfilter.conf ; /sbin/ipf -Fa && /sbin/ipf -f /etc/ipfilter.conf
+ */
+#define COMMAND_FLUSH      "TMP=`mktemp /tmp/ipfconf.XXXXX` && " AWK " 'BEGIN { copy = 1 } /^##sshguard-begin##$/ { print $0 ; copy = 0 } /^##sshguard-end##$/ { copy = 1 } copy' <" IPFILTER_CONFFILE " >$TMP ; mv $TMP " IPFILTER_CONFFILE " ; " IPFPATH "/ipf -Fa && " IPFPATH "/ipf -f " IPFILTER_CONFFILE
+
 #endif
